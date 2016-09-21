1f3b8dbff4d4843c022bfad5203a48f684249288
Jelte van Tartwijk <jlmtartw@avans.nl>
2016-09-21 14:10:54 +0200

    added apikey

---
+++ modules/apikey.js
@@ -0,0 +1,5 @@
+module.exports = 'AIzaSyBRasWVPpd9foAH0X92liqafmDTdZALwl8';
+
+// kan aangeroepen worden als volgt:
+//https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=<<API_KEY>>&location=<LAT, LONG>&radius=<<AFSTAND IN METERS>>&type=cafe
+   
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var key = require('../modules/apikey');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', msg: key });
 });
 
 module.exports = router;
